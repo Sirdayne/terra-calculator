22196c26145003a614e328f7d211ed41efa4b28a
Yernar <ernar@agrostream.net>
2018-05-28 17:58:40 +0600

    added first css/html/js

--- js/calculator.js
+++ js/calculator.js
@@ -1,1 +1,8 @@
-console.log(1)
+var counter = 0;
+var btnCalculator = document.getElementById('btn-calculator')
+
+btnCalculator.onclick = function(e) {
+    counter++
+    console.log(counter)
+    output.innerHTML = counter
+}
