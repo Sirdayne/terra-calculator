e2caa506a178c99f0e3f4e13defdb03752e65853
Yernar <ernar@agrostream.net>
2018-05-29 10:43:55 +0600

    js modified

--- js/calculator.js
+++ js/calculator.js
@@ -1,8 +1,24 @@
-var counter = 0;
-var btnCalculator = document.getElementById('btn-calculator')
+var sum = 0
+iconAsks = document.getElementsByClassName('icon-ask')
+console.dir(iconAsks)
 
 btnCalculator.onclick = function(e) {
-    counter++
-    console.log(counter)
-    output.innerHTML = counter
+    sum = parseInt(inputSowingArea.value) + parseInt(inputRealizationTonne.value)
+    output.innerHTML = sum ? sum : 'данные не заполнены' 
+}
+
+var OFFSET = 5
+
+for (var i = 0; i < iconAsks.length; i++) {
+    iconAsks[i].onclick = function(e) {
+        mouseTooltip.innerHTML = this.innerHTML
+        mouseTooltip.style.display = 'block'
+        mouseTooltip.style.top = e.clientY + OFFSET + 'px'
+        mouseTooltip.style.left = e.clientX + OFFSET + 'px'
+        console.log(e)
+    }
+    iconAsks[i].onmouseout = function(e) {
+        mouseTooltip.innerHTML = ''
+        mouseTooltip.style.display = 'none'
+    }
 }
